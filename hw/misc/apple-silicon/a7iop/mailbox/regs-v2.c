d58d277a6aaf6222de7aedf1c5ebecb26aecccba
Visual Ehrmanntraut <30368284+VisualEhrmanntraut@users.noreply.github.com>
2026-03-12 23:33:24 +0200

    fix(a7iop/mailbox/regs-v2): fix ntrungism of INT_MASK_CLR read value
    verified by hardware probing

--- hw/misc/apple-silicon/a7iop/mailbox/regs-v2.c
+++ hw/misc/apple-silicon/a7iop/mailbox/regs-v2.c
@@ -97,9 +97,8 @@ static uint64_t apple_a7iop_v2_mailbox_reg_read(void *opaque, hwaddr addr,
 
     switch (addr) {
     case REG_INT_MASK_SET:
-        return apple_a7iop_mailbox_get_int_mask(s);
     case REG_INT_MASK_CLR:
-        return ~apple_a7iop_mailbox_get_int_mask(s);
+        return apple_a7iop_mailbox_get_int_mask(s);
     case REG_IOP_CTRL:
         return apple_a7iop_mailbox_get_iop_ctrl(s);
     case REG_AP_CTRL:
